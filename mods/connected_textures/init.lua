bb49a0f1a4690853d8f273a9e8230a5d8c027295
Pitriss <devnull@wjake.com>
2014-11-24 23:35:12 +0100

    Connected textures: removed unused files, restoring of default ice look

--- mods/connected_textures/init.lua
+++ mods/connected_textures/init.lua
@@ -41,7 +41,6 @@ minetest.register_node(":default:ice", {
 	description = "Ice",
 	tiles = {"connected_textures_ice.png"},
 	is_ground_content = true,
-	use_texture_alpha = true,
 	paramtype = "light",
 	groups = {cracky=3},
 	sounds = default.node_sound_glass_defaults(),
